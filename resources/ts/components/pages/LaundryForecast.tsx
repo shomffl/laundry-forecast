192dee75896f8bc837ccc164456e90f120474dcb
sho watanabe <showatanabe0226@gmail.com>
2022-02-26 15:17:45 +0900

    [add]地域選択画面とデータ表示画面を追加

---
+++ resources/ts/components/pages/LaundryForecast.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LaundryForecast = () => {
+    return <div>LaundryForecast</div>;
+};
+
+export default LaundryForecast;
---
+++ resources/ts/components/pages/SelectLocation.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectLocation = () => {
+    return <div>SelectLocation</div>;
+};
+
+export default SelectLocation;
