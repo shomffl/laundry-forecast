661c8f98aacc68345a9d6016d511432001cdacc4
sho watanabe <showatanabe0226@gmail.com>
2022-02-27 15:25:58 +0900

    [add]取得してきた地域名をテーブル形式で表示

--- public/js/app.js
+++ public/js/app.js
@@ -1841,18 +1841,51 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 /* harmony import */ var axios__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(axios__WEBPACK_IMPORTED_MODULE_1__);
 /* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/index.js");
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 
 
 
 
 var SelectLocation = function SelectLocation() {
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]),
+      _useState2 = _slicedToArray(_useState, 2),
+      locations = _useState2[0],
+      setLocations = _useState2[1];
+
   var navigate = (0,react_router_dom__WEBPACK_IMPORTED_MODULE_2__.useNavigate)();
+  var data_list = ["札幌", "函館", "横浜", "相模原", "八千代", "大阪"];
   (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {
     axios__WEBPACK_IMPORTED_MODULE_1___default().get("http://127.0.0.1:8000/locations/get").then(function (res) {
-      return console.log(res.data.locations_data);
+      setLocations(res.data.locations_data);
     });
-  });
-  return react__WEBPACK_IMPORTED_MODULE_0__.createElement("div", null, "SelectLocation");
+  }, []);
+  console.log(locations);
+  return react__WEBPACK_IMPORTED_MODULE_0__.createElement(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0__.createElement("table", null, react__WEBPACK_IMPORTED_MODULE_0__.createElement("tbody", null, react__WEBPACK_IMPORTED_MODULE_0__.createElement("tr", null, Object.values(locations).map(function (data, key) {
+    return react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
+      key: key
+    }, data["name"]);
+  })), react__WEBPACK_IMPORTED_MODULE_0__.createElement("tr", null, Object.values(locations).map(function (data, key) {
+    return react__WEBPACK_IMPORTED_MODULE_0__.createElement("td", {
+      key: key
+    }, react__WEBPACK_IMPORTED_MODULE_0__.createElement("input", {
+      type: "checkbox",
+      value: data["name"],
+      onChange: function onChange(e) {
+        return console.log(e.target.value);
+      }
+    }));
+  })))));
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (SelectLocation);
--- resources/ts/components/pages/SelectLocation.tsx
+++ resources/ts/components/pages/SelectLocation.tsx
@@ -3,13 +3,44 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const SelectLocation = () => {
+    const [locations, setLocations] = useState<string[]>([]);
     const navigate = useNavigate();
+    const data_list = ["札幌", "函館", "横浜", "相模原", "八千代", "大阪"];
+
     useEffect(() => {
-        axios
-            .get("http://127.0.0.1:8000/locations/get")
-            .then((res) => console.log(res.data.locations_data));
-    });
-    return <div>SelectLocation</div>;
+        axios.get("http://127.0.0.1:8000/locations/get").then((res) => {
+            setLocations(res.data.locations_data);
+        });
+    }, []);
+
+    console.log(locations);
+
+    return (
+        <>
+            <table>
+                <tbody>
+                    <tr>
+                        {Object.values(locations).map((data: any, key: any) => (
+                            <td key={key}>{data["name"]}</td>
+                        ))}
+                    </tr>
+                    <tr>
+                        {Object.values(locations).map((data: any, key: any) => (
+                            <td key={key}>
+                                <input
+                                    type="checkbox"
+                                    value={data["name"]}
+                                    onChange={(e) =>
+                                        console.log(e.target.value)
+                                    }
+                                />
+                            </td>
+                        ))}
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    );
 };
 
 export default SelectLocation;
