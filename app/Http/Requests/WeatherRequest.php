f43dc281a20542a632384e585c779d0133d91536
sho watanabe <showatanabe0226@gmail.com>
2022-03-05 12:34:24 +0900

    [update]取得するデータの数の制限を排除

--- app/Http/Requests/WeatherRequest.php
+++ app/Http/Requests/WeatherRequest.php
@@ -34,7 +34,7 @@ class WeatherRequest extends FormRequest
         $client = new Client();
         $base_url = "http://api.openweathermap.org/data/2.5/forecast/";
         $api_key = config("services.weather_forecast.key");
-        $url = $base_url . "?id=" . $location_id . "&lang=ja&cnt=3&units=metric&appid=" . $api_key;
+        $url = $base_url . "?id=" . $location_id . "&lang=ja&units=metric&appid=" . $api_key;
 
         $response = $client->request("POST", $url);
 
